5b8c8962ec571db33869812041ce7fd0e1f6c783
aa104 <aa104@rice.edu>
2020-02-05 01:11:09 -0600

    spring mvc

--- src/main/java/app/controller/AppController.java
+++ src/main/java/app/controller/AppController.java
@@ -2,19 +2,32 @@ package app.controller;
 
 import app.model.AppModel;
 import app.view.AppView;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
 
-public class AppController {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-    private AppModel model;
-    private AppView view;
+public class AppController extends AbstractController {
 
-    public AppController() {
-        this.model = new AppModel();
-        this.view = new AppView();
+//    private AppModel model;
+//    private AppView view;
+
+//    public AppController() {
+//        this.model = new AppModel();
+//        this.view = new AppView();
+//    }
+
+    @Override
+    protected ModelAndView handleRequestInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        ModelAndView model = new ModelAndView("HelloWorldPage");
+        model.addObject("msg", "hello world");
+
+        return model;
     }
 
-    public void start() {
-        model.start();
-        view.start();
-    }
+//    public void start() {
+//        model.start();
+//        view.start();
+//    }
 }
